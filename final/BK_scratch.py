1676553df4f6a947de9fa19c864de0bb11c8b42d
oscar-shih <b08502141@ntu.edu.tw>
2022-12-02 18:11:45 +0800

    Finish BK merge

--- final/BK_scratch.py
+++ final/BK_scratch.py
@@ -3,7 +3,8 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
-import networkx
+import networkx as nx
+import powerlaw
 
 def readfeaturelist(filename):
     with open(filename) as f:
@@ -94,7 +95,21 @@ def BK_scratch_outputfile(userid, ref_profile, circles, profile, features, out_p
             for char in out:
                 print(char, file=f)
             print("\n", file=f)
+def read_nodeadjlist(filename):
+  G = nx.Graph()
+  for line in open(filename):
+    e1, es = line.split(':')
+    # Add a node for the user.
+    G.add_node(int(e1))
+    es = es.split()
+    for e in es:
+      if e == e1: continue
+      G.add_edge(int(e1),int(e))
+  return G
 
+def drawGraph(G):
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos)
 
 if __name__ == '__main__':
     data_dir = "./data"
@@ -112,4 +127,79 @@ if __name__ == '__main__':
         features=features,
         out_path=out_path
     ) # Generate file called "BK_scratch.txt"
-    
+
+    trainingfiles = os.listdir('./data/Training/')
+    alltraining = {}
+    for item in trainingfiles:
+        ego = int((item.split('.')[0]))
+        alltraining[ego] = readcircle(ego)
+    circlesizes = []
+    for ego in alltraining:
+        for circle in alltraining[ego]:
+            circlesizes.append(len(alltraining[ego][circle]))
+
+    n, bins, patches = plt.hist(circlesizes, 50)
+    fit = powerlaw.Fit(circlesizes, xmin=1.0)
+    for fitname in fit.supported_distributions:
+        if fit.supported_distributions[fitname]: 
+            print(fitname, fit.distribution_compare('power_law', fitname ,normalized_ratio=True))
+    fig = fit.plot_pdf(color='b', linewidth=2)
+    fit.power_law.plot_pdf(color='b', linestyle='--', ax=fig)
+    fit.truncated_power_law.plot_pdf(color='r', linestyle='--', ax=fig)
+    fig.figure.savefig("test.png")
+
+    ref_user=239
+    ref_circle = alltraining[ref_user]
+    ref_circle_nums = [c for c in ref_circle]
+    # ref_circle_nums = ref_circle.keys()
+
+    print('User:',ref_user,'\nCircle #\'s:', ref_circle_nums)
+    G = read_nodeadjlist('./data/egonets/'+str(ref_user)+'.egonet')
+    pos = nx.spring_layout(G)
+    plt.figure(figsize=(15, 15))
+    for (n, c) in enumerate(ref_circle_nums):
+        plt.subplot(2, 2, n+1)
+        nx.draw(G, pos, node_color='r', node_size=5)  # draw the background graph as red
+        nx.draw(G.subgraph(ref_circle[c]), pos, node_color='b', node_size=5)  # draw each subgraph as blue
+        plt.title(f"Egonet of User{ref_user}")
+    plt.savefig(f"final.png")  # plot each as a separate plot
+    clust = nx.clustering(G)
+    fig = plt.figure(figsize=(8,3))
+    plt.hist(clust.values(), bins=20)
+    plt.savefig("Histogram.png")
+
+    friendly_users = [user_id for user_id, coeff in clust.items() if coeff == 1]
+    fig = plt.figure(figsize=(5,5))
+    plt.title("Friendly_users")
+    nx.draw(G, pos, node_color='r', node_size=5)  # draw the background graph as red
+    nx.draw(G.subgraph(friendly_users),pos,node_color='b', node_size=5)
+    # plt.show()
+
+
+    ref_user = 345
+    ref_circle = alltraining[ref_user]
+    ref_circle_nums = [c for c in ref_circle]
+    G = read_nodeadjlist('./data/egonets/'+str(ref_user)+'.egonet')
+    pos = nx.spring_layout(G) # positions for all nodes
+
+    ncolormap = {}
+    fig = plt.figure(figsize=(20,20))
+    for c in ref_circle:
+        ncolormap[c] = (random.random(),random.random(),random.random())
+        print(ncolormap[c])
+        nx.draw_networkx_nodes(
+            G,
+            pos,
+            nodelist=ref_circle[c],
+            node_color=ncolormap[c],
+            node_size=15,
+            alpha=0.8
+        )
+    #edges
+    nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
+    plt.savefig("Final.png")
+    plt.show()
+
+
+
+
